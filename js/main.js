e88e548068aa79452865506808b8cb9e2acafa17
Yuri-tm <yuritm.tm@gmail.com>
2025-08-16 12:35:07 +0300

    Создает модуль для большой картинки

--- js/main.js
+++ js/main.js
@@ -2,4 +2,4 @@ import { getImgDescriptionArray } from './data-gen.js';
 import { checkLength } from './functions.js';
 import { isPalindrome } from './functions.js';
 import './thumbnails.js';
-
+//import './render-photo.js';
